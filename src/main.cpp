7f7415e6b760b9ab6dd407bc6c77a5aa115e63dd
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2015-10-27 23:09:31 -0500

    Add player movement functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include "libtcod.hpp"
+#include <iostream>
+
 int main() {
+	// Initialize player variables
+	int playerx = 40, playery = 25;
+	
+	// Create the console
 	TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
+	
+	// Start the processing loop
 	while ( !TCODConsole::isWindowClosed() ) {
-		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, NULL, NULL);
+		// Create new object of type key		
+		TCOD_key_t key;
+		
+		// Check for key press
+		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
+		switch (key.vk) {
+			case TCODK_UP : playery--; break;
+			case TCODK_DOWN : playery++; break;
+			case TCODK_LEFT : playerx--; break;
+			case TCODK_RIGHT : playerx++; break;
+			default : break;
+		}
+		
+		// Clear the console and update player position		
 		TCODConsole::root -> clear();
-		TCODConsole::root -> putChar(40, 25, '@');
+		TCODConsole::root -> putChar(playerx, playery, '@');
 		TCODConsole::flush();
 	}
 	return 0;
